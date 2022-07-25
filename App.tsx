bff1eec109494c09a2caf20e1379b3ea155e6978
joaoramos28 <joaogabrielramos28@outlook.com>
2022-07-25 20:36:55 -0300

    🎨  feat: Structuring folders

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { Home } from "./src/Screens";
 export default function App() {
   return (
     <NativeBaseProvider>
+      <StatusBar style="dark" />
       <Home />
     </NativeBaseProvider>
   );

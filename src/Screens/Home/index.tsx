35bd8edccc91f90fd23c85025cc61af9bff66624
joaoramos28 <joaogabrielramos28@outlook.com>
2022-07-26 20:57:54 -0300

    ✨  feat: Creating Home navbar

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,8 +1,40 @@
-import { Box } from "native-base";
+import { Avatar, Box, Container, Heading } from "native-base";
 import React from "react";
+import { WeekCalendar } from "./Components/WeekCalendar";
 
 const Home = () => {
-  return <Box>Hello world</Box>;
+  return (
+    <>
+      <Box
+        bg={"indigo.500"}
+        w={"100%"}
+        h={"250px"}
+        borderBottomLeftRadius={"20px"}
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        p={"20px"}
+      >
+        <Box
+          w={"100%"}
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          flexDirection={"row"}
+        >
+          <Heading color={"white"}>Minhas tarefas</Heading>
+
+          <Avatar>
+            <Avatar.Badge bg="green.500" />
+          </Avatar>
+        </Box>
+
+        <Box w={"100%"} marginTop="20px">
+          <WeekCalendar />
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default Home;

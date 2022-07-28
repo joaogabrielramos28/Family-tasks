0078ae54d6c2469192c76332e1291c815c9e997b
joaoramos28 <joaogabrielramos28@outlook.com>
2022-07-27 21:00:58 -0300

    🐛  fix: Scroll and adjustment in header

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,69 +1,69 @@
-import { Avatar, Box, Fab, Heading, Icon } from "native-base";
+import { Avatar, Box, Heading, ScrollView } from "native-base";
 import React from "react";
 import { WeekCalendar } from "./Components/WeekCalendar";
-import { AntDesign, FontAwesome5 } from "@expo/vector-icons";
 import { ProgressChart } from "./Components/ProgressChart";
 import { TasksStatistics } from "./Components/TasksStatistics";
-import { Dimensions } from "react-native";
+import { getBottomSpace } from "react-native-iphone-x-helper";
 
 const Home = () => {
-  const { height } = Dimensions.get("window");
   return (
-    <Box bg={"warmGray.900"} h={height}>
-      <Box
-        bg={"indigo.500"}
-        w={"100%"}
-        h={"250px"}
-        borderBottomLeftRadius={"20px"}
-        display={"flex"}
-        alignItems={"center"}
-        justifyContent={"center"}
-        p={"20px"}
-      >
+    <ScrollView bg={"warmGray.900"} flex={1}>
+      <Box>
         <Box
+          bg={"indigo.500"}
           w={"100%"}
+          h={"190px"}
+          borderBottomLeftRadius={"20px"}
           display={"flex"}
           alignItems={"center"}
-          justifyContent={"space-between"}
-          flexDirection={"row"}
+          justifyContent={"center"}
+          p={"20px"}
         >
-          <Heading color={"white"}>Minhas tarefas</Heading>
+          <Box
+            w={"100%"}
+            display={"flex"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            flexDirection={"row"}
+          >
+            <Heading color={"white"}>Minhas tarefas</Heading>
 
-          <Avatar>
-            <Avatar.Badge bg="green.500" />
-          </Avatar>
+            <Avatar>
+              <Avatar.Badge bg="green.500" />
+            </Avatar>
+          </Box>
+
+          <Box w={"100%"} marginTop="5px">
+            <WeekCalendar />
+          </Box>
         </Box>
 
-        <Box w={"100%"} marginTop="20px">
-          <WeekCalendar />
+        <Box marginTop={6} w={"100%"} display={"flex"} alignItems={"center"}>
+          <Heading color={"light.100"}>Tasks completadas </Heading>
+        </Box>
+
+        <ProgressChart />
+
+        <Box
+          display={"flex"}
+          flexDirection={"row"}
+          justifyContent={"space-around"}
+        >
+          <TasksStatistics
+            title="Tasks diárias"
+            icon={"pen"}
+            value={8}
+            color={"yellow.600"}
+          />
+          <TasksStatistics
+            title="Tasks ativas"
+            icon={"toggle-on"}
+            color={"success.400"}
+            value={12}
+          />
         </Box>
       </Box>
-
-      <Box marginTop={6} w={"100%"} display={"flex"} alignItems={"center"}>
-        <Heading color={"light.100"}>Tasks completadas </Heading>
-      </Box>
-
-      <ProgressChart />
-
-      <Box
-        display={"flex"}
-        flexDirection={"row"}
-        justifyContent={"space-around"}
-      >
-        <TasksStatistics
-          title="Tasks diárias"
-          icon={"pen"}
-          value={8}
-          color={"yellow.600"}
-        />
-        <TasksStatistics
-          title="Tasks ativas"
-          icon={"toggle-on"}
-          color={"success.400"}
-          value={12}
-        />
-      </Box>
-    </Box>
+    </ScrollView>
   );
 };
 

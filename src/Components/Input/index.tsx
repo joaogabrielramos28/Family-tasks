ddc80717d518c6c0def5af025e9df22a9b056f6d
joaoramos28 <joaogabrielramos28@outlook.com>
2022-08-02 23:26:20 -0300

    ♿️ feat: change borderColor when input is focused

--- src/Components/Input/index.tsx
+++ src/Components/Input/index.tsx
@@ -7,9 +7,13 @@ const Input = ({ ...rest }: IInputProps) => {
       {...rest}
       color={"light.50"}
       borderWidth={0}
-      borderBottomWidth={1}
+      borderBottomWidth={2}
       borderBottomColor={"warmGray.400"}
       marginBottom={2}
+      _focus={{
+        borderBottomColor: "violet.500",
+        backgroundColor: "transparent",
+      }}
     />
   );
 };

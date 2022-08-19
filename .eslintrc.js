8ec07329da700930af61f67276d05d1bfc801f8e
joaoramos28 <joaogabrielramos28@outlook.com>
2022-08-19 18:49:52 -0300

    🎨  lint: configure eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'prettier'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'react-native',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+    'react-native/no-single-element-style-arrays': 2
+
+  }
+}

e09f61a8565ea3f149132d6500870c231cb9e2e6
joaoramos28 <joaogabrielramos28@outlook.com>
2022-08-27 01:54:04 -0300

    🐛 fix:  my groups

--- src/Screens/GroupList/Components/MyGroup/index.tsx
+++ src/Screens/GroupList/Components/MyGroup/index.tsx
@@ -1,69 +1,65 @@
-import {FlatList, Text, VStack} from 'native-base';
+import {Box, Text, VStack} from 'native-base';
 import React, {useEffect, useState} from 'react';
 import {GroupCard} from '../GroupCard';
 import firestore from '@react-native-firebase/firestore';
 import {IGroupDto} from '../../../../DTOs/GroupDto';
 import {ActivityIndicator} from 'react-native';
+import {useAuth} from '../../../../hooks';
 
 const MyGroup = () => {
-  const [myGroups, setMyGroups] = React.useState<IGroupDto[]>([]);
+  const {user} = useAuth();
+  const [myGroups, setMyGroups] = React.useState<IGroupDto>({} as IGroupDto);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const subscribe = firestore()
-      .collection('Groups')
-      .onSnapshot(querySnapshot => {
-        const groups = querySnapshot.docs.map(async doc => {
-          const group = doc.data();
-          const members = await Promise.all(
-            group.members.map(async doc => {
-              return await doc.get();
-            }),
-          );
-          const membersGroups = members.map(doc => ({
-            id: doc.id,
-            ...doc.data(),
-          }));
+    firestore()
+      .collection('Users')
+      .doc(user.uid)
+      .get()
+      .then(member => {
+        if (member.data().groupInfo.id === undefined) {
+          setMyGroups({} as IGroupDto);
+          setLoading(false);
+        } else {
+          const groupID = member.data().groupInfo.id;
+          const subscribe = firestore()
+            .collection('Groups')
+            .doc(groupID)
+            .onSnapshot(async querySnapshot => {
+              const group = querySnapshot.data() as IGroupDto;
+              const members = await Promise.all(
+                group.members.map(async doc => {
+                  return doc;
+                }),
+              );
+              const membersGroups = members.map(doc => ({
+                id: doc.id,
+                ...doc,
+              }));
 
-          return {
-            id: doc.id,
-            ...doc.data(),
-            members: membersGroups,
-          };
-        }) as unknown as IGroupDto[];
-        Promise.all(groups)
-          .then(group => {
-            setLoading(false);
-            setMyGroups(group);
-          })
-          .catch(e => console.log(e));
-      });
-
-    return () => subscribe();
-  }, []);
+              setMyGroups({id: group.id, ...group, members: membersGroups});
+              setLoading(false);
+            });
+          return () => subscribe();
+        }
+      })
+      .catch(e => console.log(e));
+  }, [user.uid]);
 
   if (loading) {
     return <ActivityIndicator />;
   }
   return (
     <>
-      {myGroups.length > 0 ? (
-        <FlatList
-          marginTop={10}
-          data={myGroups}
-          contentContainerStyle={{
-            paddingHorizontal: 20,
-          }}
-          keyExtractor={item => item.id}
-          renderItem={({item}) => (
-            <GroupCard
-              id={item.id}
-              name={item.name}
-              description={item.description}
-              members={item.members}
-            />
-          )}
-        />
+      {myGroups.id ? (
+        <Box marginTop={10} paddingX={6}>
+          <GroupCard
+            id={myGroups.id}
+            name={myGroups.name}
+            description={myGroups.description}
+            members={myGroups.members}
+          />
+        </Box>
       ) : (
         <VStack alignItems={'center'} justifyContent={'center'} marginTop={20}>
           <Text color={'light.300'} fontSize={'xl'}>
